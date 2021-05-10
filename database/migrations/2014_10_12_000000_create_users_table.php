dbbafd8d9a43b213c7c4345938c443b7aa1e0d6d
Ahmed <ahmed.udhi@gmail.com>
2021-05-10 22:03:24 +0500

    Minor changes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->binary('is_admin')->default(0);
+            $table->boolean('is_admin')->default(0);
             $table->timestamps();
         });
     }
